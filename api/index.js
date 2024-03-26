49fc87e7b993e6bf9a51b58c100a6113cb04273f
kavipriya-sugumar <kavipriya.sugumar@digamend.com>
2024-03-26 17:23:13 +0530

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -25,3 +25,17 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRoutes)
 app.use('/api/auth', authRoutes )
+
+
+
+//middleware to handle and display errors
+app.use((err, req,res, next)=>{
+    const statusCode= err.statuscode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+
+})
