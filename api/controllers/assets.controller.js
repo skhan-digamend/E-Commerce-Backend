96a8b0b13d99b69bcd5f6a7bcc7cff1d5c76d704
kavipriya-sugumar <kavipriya.sugumar@digamend.com>
2024-04-01 10:32:42 +0530

    delete category api and functionality

--- api/controllers/assets.controller.js
+++ api/controllers/assets.controller.js
@@ -1,51 +1,61 @@
 import Asset from "../models/assets.model.js";
-import { errorHandler } from "../utils/error.js"
-
-
+import { errorHandler } from "../utils/error.js";
 
 //functionality for create category
-export const createCategory = async(req,res,next)=>{
-    const title=req.body;
-    if(!req.user.isAdmin){
-        return next(errorHandler(403,'You are not allowed to create a asset'))
-    }
-    if(!title){
-        return next(errorHandler(400, 'Please provide all the required fields '))
-    }
-    const slug = req.body.title
-    .split(' ')
-    .join('-')
+export const createCategory = async (req, res, next) => {
+  const title = req.body;
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a asset"));
+  }
+  if (!title) {
+    return next(errorHandler(400, "Please provide all the required fields "));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
     .toLowerCase()
-    .replace(/[^a-zA-Z0-9-]/g, '');
-    const newCategory = new Asset({
-        ...req.body,
-        slug,
-        userId: req.user.id,
-    });
-    try{
-        const savedCategory = await newCategory.save();
-        res.status(201).json(savedCategory);
-    } catch(error){
-        next(error);
-
-    }
+    .replace(/[^a-zA-Z0-9-]/g, "");
+  const newCategory = new Asset({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedCategory = await newCategory.save();
+    res.status(201).json(savedCategory);
+  } catch (error) {
+    next(error);
+  }
 };
 
 //functionality for get category
 
-export const getCategory = async(req,res,next)=>{
-    try {
-        const allCategory =await Asset.find({
-            ...(req.query.userId && {userId: req.query.userId}),
-            ...(req.query.title && {title: req.query.title}),
-            ...(req.query.categoryId && {_id: req.query.categoryId})
-        } )
-        res.status(200).json({
-            allCategory
+export const getCategory = async (req, res, next) => {
+  try {
+    const allCategory = await Asset.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.title && { title: req.query.title }),
+      ...(req.query.categoryId && { _id: req.query.categoryId }),
+    });
+    res.status(200).json({
+      allCategory,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-        })  
-    } catch (error) {
-        next(error);
-        
-    }
-}
+export const deleteCategory = async (req, res, next) => {
+  if(!req.user.isAdmin || req.user._id !== req.params.userId){
+    return next(errorHandler(403, 'You are not allowed to delete this category'));
+  }
+  try {
+    await Asset.findByIdAndDelete(req.params.categoryId);
+    res.status(200).json('The post has been deleted');
+    
+  } catch (error) {
+    next(error);
+    
+  }
+
+};
--- api/routes/assets.route.js
+++ api/routes/assets.route.js
@@ -2,10 +2,13 @@ import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import { createCategory } from '../controllers/assets.controller.js';
 import { getCategory } from '../controllers/assets.controller.js';
+import { deleteCategory } from '../controllers/assets.controller.js';
 
 const router= express.Router();
 
 router.post('/createCategory',verifyToken, createCategory);
 router.get('/getCategory',getCategory);
+router.delete('/deleteCategory/:categoryId',verifyToken,deleteCategory);
+
 
 export default router;
