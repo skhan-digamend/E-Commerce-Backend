bd664759b3b977dffe511ab85b532ebf30e5c03f
kavipriya-sugumar <kavipriya.sugumar@digamend.com>
2024-04-03 12:32:18 +0530

    wishlist model created

--- api/controllers/asset.controller.js
+++ api/controllers/asset.controller.js
@@ -1,6 +1,7 @@
 import Asset from "../models/asset.model.js";
 import { errorHandler } from "../utils/error.js";
 
+//to craete asset
 export const createAsset = async (req, res, next) => {
     const {assetName,Id,fileSize,fileFormat,price} = req.body;
     if (!req.user.isAdmin) {
@@ -90,6 +91,61 @@ export const createAsset = async (req, res, next) => {
       next(error);
     }
   };
+
+
+
+  //wishlist
+
+//   const express = require('express');
+// const router = express.Router();
+// const Wishlist = require('../models/Wishlist');
+// const Product = require('../models/Product');
+// const User = require('../models/User');
+
+// // Add a product to the user's wishlist
+// router.post('/like', async (req, res) => {
+//     try {
+//         const { userId, productId } = req.body;
+
+//         // Check if the user exists
+//         const user = await User.findById(userId);
+//         if (!user) {
+//             return res.status(404).json({ error: 'User not found' });
+//         }
+
+//         // Check if the product exists
+//         const product = await Product.findById(productId);
+//         if (!product) {
+//             return res.status(404).json({ error: 'Product not found' });
+//         }
+
+//         // Check if the product is already in the wishlist
+//         const wishlist = await Wishlist.findOne({ userId });
+//         if (wishlist && wishlist.products.includes(productId)) {
+//             return res.status(400).json({ error: 'Product already in wishlist' });
+//         }
+
+//         // If wishlist doesn't exist, create a new one
+//         if (!wishlist) {
+//             const newWishlist = new Wishlist({
+//                 userId,
+//                 products: [productId]
+//             });
+//             await newWishlist.save();
+//             return res.status(201).json(newWishlist);
+//         }
+
+//         // Add the product to the wishlist
+//         wishlist.products.push(productId);
+//         await wishlist.save();
+//         res.status(200).json(wishlist);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({ error: 'Server Error' });
+//     }
+// });
+
+// module.exports = router;
   
 
   
---
+++ api/models/wishlist.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const wishlistSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+    },
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'asset'
+    }]
+    
+  },
+  { timestamps: true }
+);
+
+const wishList = mongoose.model("wishlist", wishlistSchema);
+
+export default wishList;
