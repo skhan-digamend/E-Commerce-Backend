63acc800d44401f12a586a539c4806f25691c198
kavipriya-sugumar <kavipriya.sugumar@digamend.com>
2024-04-08 13:58:53 +0530

    user idea code updated

--- api/routes/category.route.js
+++ api/routes/category.route.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.post("/createCategory", verifyToken, createCategory);
 router.get("/getCategory", getCategory);
-router.get("/getCategory/:id",getCategoryById)
+router.get("/getCategory/:id",getCategoryById) ///api/asset/getAssets?category=660a94246f6151fd1a190a6d
 router.delete("/deleteCategory/:categoryId", verifyToken, deleteCategory);
 router.put("/editCategory/:categoryId", verifyToken, editCategory);
 
